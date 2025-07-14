3d19a071778dec342ab77d8e8ecf9c98fca29d0b
tonrl <hoswt4s3321399@gmail.com>
2025-07-14 14:03:54 +0900

    Minor Change nvim config

--- lua/schlau-nvim/init.lua
+++ lua/schlau-nvim/init.lua
@@ -7,8 +7,6 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 
-
-
 --vim.api.nvim_create_autocmd("FileType", {
 --        pattern = {"markdown", "rust" },
 --        callback = function()
